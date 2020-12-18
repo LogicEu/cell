a297ac26f9c7c2a06d9178090258affba916f2c4
LogicEu <76014070+LogicEu@users.noreply.github.com>
2020-12-18 19:35:52 -0300

    Add files via upload

---
+++ cell.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include "keyboard.h"
+
+int main(int argc, char* argv[])
+{
+    srand(time(NULL));
+    unsigned int w = 24, h = 52, count = 0, restart = 0;
+    if (argc == 2) w = atoi(argv[1]);
+    else if (argc > 2) 
+    {
+        w = atoi(argv[1]);
+        h = atoi(argv[2]);
+    }
+
+    const unsigned int width = w, height = h;
+    int cells[width][height], bufferCells[width][height];
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            bufferCells[x][y] = rand() % 2;
+        }
+    }
+    //Start loop
+    while(1)
+    {
+        system("clear");
+        printf("-------------TERMINAL CELULAR AUTOMATA-------------\n");  
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                count = 0;
+                cells[x][y] = bufferCells[x][y];
+
+                //Calculate next state for the grid
+                if (x + 1 < width && cells[x+1][y] == 1) count ++;
+                if (x + 1 < width && y - 1 >= 0 && cells[x+1][y-1] == 1) count ++;
+                if (y - 1 >= 0 && cells[x][y-1] == 1) count ++;
+                if (x - 1 >= 0 && y - 1 >= 0 && cells[x-1][y-1] == 1) count ++;
+                if (x - 1 >= 0 && cells[x-1][y] == 1) count ++;
+                if (x - 1 >= 0 && y + 1 < height && cells[x-1][y+1] == 1) count ++;
+                if (y + 1 < height && cells[x][y+1] == 1) count ++;
+                if (x + 1 < width && y + 1 < height && cells[x+1][y+1] == 1) count ++;
+
+                if (restart == 1)
+                {
+                    bufferCells[x][y] = rand() % 2;
+                }
+                else if (cells[x][y] == 0)
+                {
+                    if (count == 3) bufferCells[x][y] = 1;
+                    else bufferCells[x][y] = 0;
+                    printf(" ");
+                }
+                else 
+                {
+                    if (count >= 2 && count <= 3) bufferCells[x][y] = 1;
+                    else bufferCells[x][y] = 0;
+                    printf("▒");
+                }
+            }
+            printf("\n");
+        }
+        printf("-- Press 'ESC' or 'q' to exit and 'r' to restart. --\n");
+        restart = 0;
+        int key = keysPressed();
+        if (key == 27 || key == 113) return 0;
+        if (key == 114) restart = 1;
+        usleep(50000); 
+    }
+    return 0;
+}
---
+++ keyboard.h
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <termios.h>
+
+//Get keyoard input form terminal from loop
+
+int keyboardHit()
+{
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if(ch != EOF)
+    {
+        ungetc(ch, stdin);
+        return 1;
+    }
+    return 0;
+}
+
+char getch()
+{
+    char c;
+    system("stty raw");
+    c = getchar();
+    system("stty sane");
+    return c;
+}
+
+int keysPressed()
+{
+    int pressed;
+    if(keyboardHit())
+    {
+        pressed = getch();
+        return pressed;
+    }
+    return 0;
+}
